5c87a5ce58d65b326e402db5aedf335f554a2aba
jessie.hyun <jessie.hyun@yorksj.ac.uk>
2024-05-03 11:54:01 +0900

    setting

--- team_10/team_10/settings.py
+++ team_10/team_10/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'articles',
+    'comment',
+    'like',
 ]
 
 MIDDLEWARE = [
